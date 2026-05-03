d462e4307d89d4fa6a13824ab1d680e6037caf02
NamPham <nampham30062004@gmail.com>
2026-05-03 20:36:06 +0200

    feat: add main application layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { colors } from '../theme/colors';
-import { StackScreen } from 'react-native-screens';
+import React from "react";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { colors } from "../src/theme/theme";
 
 export default function Layout() {
   return (
     <>
-        <StatusBar style="light" backgroundColor={colors.primary} />
-        <Stack 
-            ScreenOptions={{
-                headerStyle: { backgroundColor: colors.background },
-                headerTintColor: colors.white,
-                headerTitleStyle: { fontWeight: 'bold' },
-            }}
-        >
-            <StackScreen name="index" options={{ title: 'Museum Search' }} /> 
-            <StackScreen name="results" options={{ title: 'Search Results' }} />
-            <StackScreen name="details" options={{ title: 'Museum Details' }} />
-        </Stack>
+      <StatusBar style="light" backgroundColor={colors.primary} />
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: colors.background },
+          headerTintColor: colors.text,
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Museum Search" }} />
+        <Stack.Screen name="results" options={{ title: "Search Results" }} />
+        <Stack.Screen name="detail" options={{ title: "Museum Details" }} />
+      </Stack>
     </>
   );
 }
